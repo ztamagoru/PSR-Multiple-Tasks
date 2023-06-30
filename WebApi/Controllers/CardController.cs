f88b8eb5af106d40b883619b9c670222ed9707fa
tamagoru <marushannnn@outlook.com>
2023-06-30 04:52:11 -0300

    Add project files.

---
+++ WebApi/Controllers/CardController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("cards")]
+
+    public class CardController: ControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult> ProcessCard([FromBody] string card)
+        {
+            var randomvalue = CardGen.NextDouble();
+            var approved = randomvalue > 0.1;
+
+            await Task.Delay(1000);
+
+            Console.WriteLine($"Card number {card} approved");
+            return Ok(new {Card = card, Approved = approved});
+        }
+    }
+}
---
+++ WebApi/Controllers/CardGen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+
+namespace WebApi.Controllers
+{
+    public class CardGen
+    {
+        private static RNGCryptoServiceProvider _global =
+            new RNGCryptoServiceProvider();
+
+        [ThreadStatic]
+        private static Random _local;
+
+        public static double NextDouble()
+        {
+            Random inst = _local;
+            if (inst == null)
+            {
+                byte[] buffer = new byte[4];
+                _global.GetBytes(buffer);
+                _local = inst = new Random(
+                    BitConverter.ToInt32(buffer, 0));
+            }
+
+            return inst.NextDouble();
+        }
+    }
+}
